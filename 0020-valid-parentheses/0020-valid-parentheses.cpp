f414799b3ee483a6ce4e1209851652191bc7a76f
Lav Raj <lavraj781@gmail.com>
2026-09-21 23:34:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (88.67%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for (char ch : s) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                // No opening bracket available
+                if (st.empty()) {
+                    return false;
+                }
+
+                char top = st.top();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        // Stack should be empty
+        return st.empty();
+    }
+};
